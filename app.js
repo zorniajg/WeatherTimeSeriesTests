cf4cf46466a4c5b0e84094c59987faa8fbb6908b
zorniajg <zorniajg@dukes.jmu.edu>
2022-07-19 17:49:43 -0400

    Adding package.json and app.js (app.js does not  do anything currently) REDONE

---
+++ app.js
@@ -0,0 +1,14 @@
+// const NormalDatabase = require('./timeSeriesDatabaseTests/normalDatabaseTests/createNormalDB');
+// const TimeseriesDatabase = require('./timeSeriesDatabaseTests/timeSeriesDatabaseTests/createTimeSeriesDB')
+
+async function main() {
+    try {
+        
+
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+main().catch(console.error);
